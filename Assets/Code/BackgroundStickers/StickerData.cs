d1427c5bbcae4be9ac41e4eb236728e889213002
wrighp <wrightCoder@gmail.com>
2018-09-28 11:12:44 -0400

    :construction: Base StickerPage mehods

--- Assets/Code/BackgroundStickers/StickerData.cs
+++ Assets/Code/BackgroundStickers/StickerData.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Serialization;
 using UnityEngine;
 using System.Collections.Generic;
 
 [Serializable]
 public class StickerData
 {
-	public Vector3 location;
-	public string sprite;
+	public Vector3 position;
+	public string sprite = "";
 }
 
 [Serializable]
 public class StickerPageData
 {
 	public List<StickerData> stickers = new List<StickerData>();
-	public string backgroundSprite;
 }
 
--- Assets/Code/BackgroundStickers/StickerPage.cs
+++ Assets/Code/BackgroundStickers/StickerPage.cs
@@ -1,37 +1,62 @@
-﻿using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Serialization;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class StickerPage : MonoBehaviour
 {
-	StickerPageData pageData;
+
+	StickerPageData pageData = new StickerPageData();
+    List<GameObject> stickers = new List<GameObject>();
 
     const string SAVE_PATH = "Stickers/";
 	
-	public void SetupPage(string stickerPageName)
+	public void LoadPage(string stickerPageName)
 	{
 		//Currently JsonLoading
 		string location = SerializationManager.CreatePath(SAVE_PATH + stickerPageName);
 		StickerPageData data = SerializationManager.LoadJsonObject<StickerPageData>(location);
-		SetupPage(data);
+		LoadPage(data);
 	}
 	
-	public void SetupPage(StickerPageData data)
+	public void LoadPage(StickerPageData data)
 	{
 		pageData = data;
 		ReloadPage();
 	}
-	
+
+    public void ClearPage()
+    {
+        foreach (GameObject g in stickers)
+        {
+            Destroy(g);
+        }
+        stickers.Clear();
+    }
+
 	public void ReloadPage()
 	{
-		
-	}
+        ClearPage();
+        //Build page based on pagedata
+        foreach (StickerData sD in pageData.stickers)
+        {
+            //Create sticker sprites in their proper locations
+            GameObject g = CreateSticker(sD.sprite);
+            g.transform.parent = transform;
+            g.transform.position = sD.position;
+            stickers.Add(g);
+        }
+    }
 	
 	void UpdatePageData()
 	{
-		
+        List<StickerData> stickerData = new List<StickerData>();
+		foreach(GameObject g in stickers)
+        {
+            StickerData sD = new StickerData();
+            sD.position = g.transform.position;
+            //Set sprite name here
+        }
+        pageData.stickers = stickerData;
 	}
 	
 	public void SavePage(string fileName)
@@ -41,6 +66,12 @@ public class StickerPage : MonoBehaviour
 		string location = SerializationManager.CreatePath(SAVE_PATH + fileName);
 		SerializationManager.SaveJsonObject(location, pageData);
 	}
-	
+
+   
+    public GameObject CreateSticker(string name)
+    {
+        throw new NotImplementedException(); 
+    }
+    
 }
 
