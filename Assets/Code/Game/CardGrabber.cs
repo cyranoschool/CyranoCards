3fcd66ab79452f2a22d6665730f1fe875958bab2
Unknown <wrightCoder@gmail.com>
2018-09-28 16:30:50 -0400

    🏃 Character movement, collision, Picking up objects

---
+++ Assets/Code/Game/CardGrabber.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardGrabber : MonoBehaviour {
+
+    [Header("Init")]
+    public Transform CardHolder;
+
+    private Transform holding;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        if(!TryPickupCard(c))
+        {
+            TryDropCard(c);
+        }
+    }
+
+    bool TryPickupCard(Collider2D c)
+    {
+        if (holding == null && Input.GetButtonDown("Jump"))
+        {
+            PickupCard card = c.GetComponent<PickupCard>();
+            if (card == null)
+            {
+                return false;
+            }
+
+            //Put card into "hand" transform and disable its collider
+            c.transform.SetParent(CardHolder);
+            c.transform.localPosition = Vector3.zero;
+            holding = c.transform;
+            c.GetComponent<BoxCollider2D>().enabled = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void TryDropCard(Collider2D c)
+    {
+
+    }
+}
---
+++ Assets/Code/Game/PlayerController2D.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController2D : MonoBehaviour
+{
+    [Header ("Init")]
+    public Transform Body;
+
+    [Header("Config")]
+    public float Acceleration = 2f;
+
+    [Header("Input Axes")] //For viewing in editor
+    [SerializeField]
+    float inX;
+    [SerializeField]
+    float inY;
+    [SerializeField]
+    Vector2 input;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Flip body of sprite depending on direction
+        if(inX != 0)
+        {
+            Body.transform.rotation = Quaternion.Euler(0, (inX < 0)? 0 : 180f, 0);
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {
+        //Consider using getaxisraw for less "floaty" input
+        inX = Input.GetAxisRaw("Horizontal");
+       inY = Input.GetAxisRaw("Vertical");
+        input = Vector3.right * inX + Vector3.up * inY;
+        input.Normalize();
+
+        //Cut off input if there isn't much of it
+        //This is to stop faulty presses or 0 input scenarios
+        float mag = Mathf.Abs(inX) + Mathf.Abs(inY);
+        float cutoff = 1;
+        if(mag < .02f)
+        {
+            cutoff = 0;
+        }
+        
+
+        rb.AddForce(input * cutoff * Acceleration);
+    }
+}
