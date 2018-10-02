a921e8a6112403fccd7d156d1f0494fb7e40ee35
Unknown <wrightCoder@gmail.com>
2018-10-02 13:12:08 -0400

    :sparkles: Line breakup into cards and their indices
    Example:
    Line to break up: a b c b a c b c
    a:                  a b c-->1 2 3     a-->1               
    b:                  b c-->2 3           b-->2               
    c:                  c-->3               
    b:                  b-->2               
    a:                  a-->1               
    c:                  c-->3               
    b:                  b c-->2 3           b-->2               
    c:                  c-->3

--- Assets/Code/Game/LineManager.cs
+++ Assets/Code/Game/LineManager.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public class LineManager : MonoBehaviour {
+public class LineManager : MonoBehaviour
+{
 
     [Header("Init Prefabs")]
     public GameObject CardDropOff;
@@ -16,38 +18,67 @@ public class LineManager : MonoBehaviour {
     public string LanguageFolder;
     public bool reloadCards = false;
 
-    List<List<string>> wordGroups;
+    List<List<CardIndexer>> wordIndices;
+
 
     // Use this for initialization
-    void Start () {
-        
-		if(reloadCards)
+    void Start()
+    {
+
+        if (reloadCards)
         {
             CardManager.LoadFolder(LanguageFolder);
         }
-        BuildWords();
-	}
+        wordIndices = BuildWords();
+        PrintWordIndices();
+    }
 
-    //WIP
-    //Creates the various sets of words and parents
-    void BuildWords()
+    void PrintWordIndices()
+    {
+        //Print wordIndices
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Line to break up: {LineString}");
+        string separator = "";
+
+        var split = LineString.Split();
+
+        for (int i = 0; i < wordIndices.Count; i++)
+        {
+            sb.Append($"{split[i]}:{separator}".PadRight(20));
+
+            var words = wordIndices[i];
+            for (int j = 0; j < words.Count; j++)
+            {
+                sb.Append($"{words[j].Card.From}-->{words[j].Card.To}{separator}".PadRight(20));
+            }
+            sb.AppendLine();
+        }
+        Debug.Log(sb.ToString());
+    }
+
+    /// <summary>
+    /// Looks for cards in LanguageFolder that match the "from" words from the LineString
+    /// </summary>
+    /// <returns>Returns a list of length n being the number of words in LineString with elements being cards that can be put in these locations</returns>
+    List<List<CardIndexer>> BuildWords()
     {
         string[] words = LineString.Split();
 
         //Each wordGroup is made up of cards with wordcount words.Length - i + 1
-        wordGroups = new List<List<string>>();
+        //Each element keeps track where it starts with CardIndexer
+        HashSet<CardIndexer> cardSet = new HashSet<CardIndexer>();
 
         //Search for largest words first and add them to list then get smaller words
         StringBuilder sb = new StringBuilder();
-        for(int wordSize = words.Length; wordSize > 0; wordSize--)
+        for (int wordSize = words.Length; wordSize > 0; wordSize--)
         {
             //The group of all words of the same length within this line
-            List<string> currentGroup = new List<string>();
-            wordGroups.Add(currentGroup);
-            for (int i = 0; i <= words.Length - wordSize; i ++)
+            //List<CardIndexer> currentGroup = new List<CardIndexer>();
+            //wordGroups.Add(currentGroup);
+            for (int i = 0; i <= words.Length - wordSize; i++)
             {
                 //Append all words together
-                for(int j = 0; j < wordSize; j++)
+                for (int j = 0; j < wordSize; j++)
                 {
                     //Concatenate all words into single string to form a key
                     //e.g. "a" "b" "c" --> "a b c"
@@ -60,33 +91,64 @@ public class LineManager : MonoBehaviour {
                 }
                 string key = sb.ToString();
                 sb.Clear();
-                //Currently appending strings and not CardData
+
+                //Get all cards that match that key and create CardIndexers for them
                 var set = CardManager.GetCardsFrom(key);
-                //Debug.Log(set.Count);
-                currentGroup.AddRange(set.Select(x => x.From));
+                var cardIndexerSet = set.Select(x => new CardIndexer(x, i, wordSize));
+                cardSet.UnionWith(cardIndexerSet);
             }
         }
-        
-        foreach(var group in wordGroups)
+
+        //Add children of cards as well
+        //Requires a Hashset so there are no duplicate CardData
+        //This is because a child may not be the same word as the lower level word
+        //
+        //The relationship between card children is still undecided
+
+        //Populate output [index][n CardData that fits]
+        List<List<CardIndexer>> slots = new List<List<CardIndexer>>(words.Length);
+        for(int i = 0; i < words.Length; i++)
         {
-            foreach(string s in group)
-            {
-                sb.Append("\t" + s);
-            }
-            sb.AppendLine();
+            slots.Add(new List<CardIndexer>());
         }
-        Debug.Log(sb.ToString());
-        
+        foreach(CardIndexer cardI in cardSet)
+        {
+            slots[cardI.Index].Add(cardI);
+        }
+        return slots;
     }
 
-    void CombineWords()
+    class CardIndexer : IEqualityComparer<CardIndexer>
+    {
+        public CardData Card;
+        public int Index;
+        public int Length;
+
+        public CardIndexer(CardData card, int index, int length)
+        {
+            this.Card = card;
+            this.Index = index;
+            this.Length = length;
+        }
+
+        //CardData are only considered the same if they have the same card AND same index
+        public bool Equals(CardIndexer x, CardIndexer y)
+        {
+            //Any carddata should have the same length, so it doesn't need to be checked
+            return x.Card == y.Card && x.Index == y.Index;
+        }
+
+        public int GetHashCode(CardIndexer obj)
+        {
+            return Card.GetHashCode() ^ Index;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
 
     }
-
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
