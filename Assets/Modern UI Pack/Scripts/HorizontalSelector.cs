f4b02ee1bb0f67f6fdb0efb23b3d4ca5c7a99267
Unknown <wrightCoder@gmail.com>
2019-01-11 13:47:30 -0500

    :sparkles: Modern UI Pack
    Bought from Asset store, to be used as a UI solution.

---
+++ Assets/Modern UI Pack/Scripts/HorizontalSelector.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+
+namespace Michsky.UI.ModernUIPack
+{
+    public class HorizontalSelector : MonoBehaviour
+    {
+        [Header("RESOURCES")]
+        public TextMeshProUGUI label;
+        private int index = 0;
+        public int defaultIndex = 0;
+
+        [Header("ELEMENTS")]
+        public List<string> elements = new List<string>();
+
+        [Header("EVENT")]
+        public UnityEvent onValueChanged;
+
+        void Start()
+        {
+            label = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+            label.text = elements[defaultIndex];
+        }
+
+        public void PreviousClick()
+        {
+            if (index == 0)
+            {
+                index = elements.Count - 1;
+            }
+
+            else
+            {
+                index--;
+            }
+
+            onValueChanged.Invoke();
+            label.text = elements[index];
+        }
+
+        public void ForwardClick()
+        {
+            if ((index + 1) >= elements.Count)
+            {
+                index = 0;
+            }
+
+            else
+            {
+                index++;
+            }
+
+            onValueChanged.Invoke();
+            label.text = elements[index];
+        }
+    }
+}
